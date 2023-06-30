550edd3eeb7728a928c2f3966d2a4ae6464c81a1
Usman Sajid <130120381+hyyperAI@users.noreply.github.com>
2023-06-30 11:27:27 +0500

    Update main.py

--- Guess word/main.py
+++ Guess word/main.py
@@ -1,25 +1,44 @@
-import random
-word_list=['ark','github','javascript','linkedin','python']
-chosen_word=random.choice(word_list)
-x=list(chosen_word)
+umport random
 
-display=[]
-for letter in range(len(chosen_word)):
-    display+='-'
-print(display)
+easy_level_guess=10
+hard_level_guess=5
+turns = 0
 
-remaining_life=6
-life=0
-while display!=x:
-    guess=input('write any word ').lower()
-    for position in range(len(chosen_word)):
-        letter=chosen_word[position]
-        if letter==guess:
-            display[position]=letter
-        elif letter!=guess:
-            life+=1
-            print(f'your life is {life}')
-    print(display)
-    if life==remaining_life:
-        break
-print('you won')
+def set_difficulty():
+    level=input("choose the level of difficulty that is 'easy' or 'hard'")
+    if level=="easy":
+        print(f"your total guesses is {easy_level_guess}")
+        return easy_level_guess
+    else :
+        print(f"your total guesses is {hard_level_guess} ")
+        return hard_level_guess
+
+def check_answer(user_guess,comp_guess,turns):
+    if user_guess>comp_guess:
+        print("too high")
+        return turns-1
+    elif user_guess<comp_guess:
+        print("too low")
+        return turns - 1
+    else:
+        print("congratulation! you won.")
+
+def game():
+    print("welcome to the guess game you have to guess a number until you guess is over")
+
+    comp_guess=random.randint(1,101)
+    print(comp_guess)
+    turns=set_difficulty() # give back the num of turns     # calling first function
+    print(f'you have {turns} turns')
+
+    user_guess=0
+    while user_guess!= comp_guess:
+        user_guess=int(input("enter your guess "))
+        turns=check_answer(user_guess,comp_guess,turns)# give us number of turns - 1 and (user,comp guess comparison) #calls second function
+
+        print(f'your remaining turns is {turns}')
+        if turns==0:
+            print('game over ! try again')
+        return
+
+game()
